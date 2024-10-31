3493a070790b91f9da25aadfff4397071dd38885
Shreyak Timsina <shreyaktimsina29@gmail.com>
2024-10-31 17:58:02 +0545

    Added Register User

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,9 +1,53 @@
 import asyncHandler from "../utils/asyncHandler.js";
+import ApiError from "../utils/ApiError.js";
+import uploadOnCloudinary from "../utils/cloudinary.js";
+import ApiResponse from "../utils/ApiResponse.js";
 
-const registerUser = asyncHandler((req, res) => {
-    res.status(200).json({
-        message: "User Registration Method Called"
+const registerUser = asyncHandler(async (req, res) => {
+    const { fullName, email, username, password } = req.body;
+    console.log(fullName, email);
+
+    if ([fullName, email, username, password].some(field => field?.trim() === "")) {
+        throw new ApiError(400, `${field} is mandatory!`)
+    }
+
+    const existedUser = User.findOne({
+        $or: [{username}, {email}]
     })
+    if (existedUser) {
+        throw new ApiError(409, "User already exists!")
+    }
+
+    const avatarLocalPath = req.files?.avatar[0]?.path;
+    const coverImageLocalPath = req.files?.avatar[0]?.path;
+    if (!avatarLocalPath) {
+        throw new ApiError(400, "Avatar Image Necessary!");
+    }
+
+    const avatar = await uploadOnCloudinary(avatarLocalPath);
+    const coverImage = await uploadOnCloudinary(coverImageLocalPath);
+
+    if (!avatar) {
+        throw new ApiError(400, "Avatar Image Necessary!");
+    }
+
+    const user = await User.create({
+        fullName,
+        email,
+        username: username.toLowerCase(),
+        password,
+        avatar: avatar.url,
+        coverImage: coverImage?.url || ""
+    })
+
+    const createdUser = await User.findbyId(user._id).select("-password - refreshToken");
+    if (!createdUser) {
+        throw new ApiError(500, "Could not register the user!")
+    }
+
+    return res.status(201).json(
+        new ApiResponse(200, createdUser, "User Registered Successfully!")
+    )
 })
 
 export default registerUser;
